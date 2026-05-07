199a0feecdf5dd551f555b36b77704b3f6fa6337
kavindup20010305 <kavindup52@gmail.com>
2026-05-07 12:02:29 +0530

    Change URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 // Paste your deployed Google Apps Script Web App URL here.
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyyQxCuxQMmroJnlOF0naA4nzqsJQkZMpcy0sLuqNhkS1pyfwtY7VHVs3308SE3Z8Bo/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbz1UPWnIfJto0nH1zPSg3GCGltT6LuxciexRDUWgqu8_Ee0L6U8sOL02zqJJ_hyaDOV/exec",
   BATCH_SIZE: 10,
   AUTO_FLUSH_INTERVAL_MS: 5000,
   MOUSEMOVE_SAMPLE_INTERVAL_MS: 120,
